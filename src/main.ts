9e13a1d2886b322a91e4c88cd2c654368d2d6bda
whoisexcel <excellence@bepeerless.co>
2024-07-06 14:28:30 +0100

    added configuration for environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const config = new DocumentBuilder()
+    .setTitle('User Management API')
+    .setDescription('The User Management API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(configService.get('PORT') || 3000);
   await app.listen(3000);
 }
 bootstrap();
