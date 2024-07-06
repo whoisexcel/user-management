67cce270f50482e7e8a838aa1169a0ffc3bc08d3
whoisexcel <excellence@bepeerless.co>
2024-07-06 14:10:11 +0100

    data source creation

---
+++ src/database/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+import { User } from '../users/user.entity';
+
+
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [User],
+    synchronize: true,
+})
