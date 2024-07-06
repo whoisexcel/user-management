f8890b6c8008890f3545faf2fc78c9a5e4a54537
whoisexcel <excellence@bepeerless.co>
2024-07-06 15:19:41 +0100

    logging implementation

---
+++ src/middleware/logger.middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction): void {
+    const { method, originalUrl } = req;
+    const start = Date.now();
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const duration = Date.now() - start;
+      this.logger.log(`${method} ${originalUrl} ${statusCode} ${duration}ms`);
+    });
+
+    next();
+  }
+}
