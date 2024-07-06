8a0c911bfc3d2764b2631d63710cceddeb662fac
whoisexcel <excellence@bepeerless.co>
2024-07-06 15:14:37 +0100

    users controller implemented

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,18 @@
+// src/auth/dto/auth-credentials.dto.ts
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(
+    /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.*\s).{8,32})/,
+    { message: 'password too weak' },
+  )
+  password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+// src/users/users.controller.ts
+import { Controller, Get, Post, Patch, Delete, Param, Body, UseGuards, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Get(':id')
+  @UseGuards(AuthGuard())
+  async getUserById(@Param('id') id: number): Promise<User> {
+    return this.usersService.getUserById(id);
+  }
+
+  @Patch(':id')
+  @UseGuards(AuthGuard())
+  async updateUser(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto): Promise<User> {
+    return this.usersService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @UseGuards(AuthGuard())
+  async deleteUser(@Param('id') id: number): Promise<void> {
+    return this.usersService.deleteUser(id);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard(), RolesGuard)
+  @Roles('admin')
+  async getUsers(@Query() filterDto: GetUsersFilterDto): Promise<User[]> {
+    return this.usersService.getUsers(filterDto);
+  }
+}
