7427dc66c697e0e574857de1bc2357f682c3ef1f
whoisexcel <excellence@bepeerless.co>
2024-07-06 14:35:59 +0100

    structure for user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  fullName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
